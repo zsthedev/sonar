2adbfee74931eff6a6cd9e61ee77aad604a807ce
techmaze24 <itstechmaze@gmail.com>
2024-07-21 03:23:45 +0500

    Loader Updated

--- script.js
+++ script.js
@@ -1,14 +1,88 @@
-const locomotiveScroll = new LocomotiveScroll()
-
 const text = document.getElementById("text");
 
-gsap.to(text, {
+const tl = gsap.timeline();
+const innerBox = document.getElementById("inner-box");
+
+const box = document.querySelectorAll(".preloader-box");
+const boxContainer = document.querySelector(".box-container");
+const hor = document.querySelector(".hor");
+const ver = document.querySelector(".ver");
+const preloader = document.querySelector(".preloader");
+
+tl.to(innerBox, {
+  rotate: 180,
+  duration: 1,
+  delay: 1,
+});
+
+tl.to(
+  box,
+  {
+    duration: 1,
+    scale: 2,
+  },
+  "a"
+);
+
+tl.to(
+  boxContainer,
+  {
+    duration: 1,
+    width: "100%",
+    height: "100vh",
+  },
+  "a"
+);
+
+tl.to(
+  hor,
+  {
+    duration: 4,
+    rotate: "-80",
+    width: "100vw",
+    height: "100vh",
+  },
+  "a"
+);
+
+tl.to(
+  boxContainer,
+  {
+    display: "none",
+    opacity: 0,
+  },
+  "a"
+);
+
+tl.to(
+  hor,
+  {
+    delay: 2,
+    duration: 4,
+    opacity: 0,
+  },
+  "a"
+);
+
+tl.to(
+  preloader,
+  {
+    delay: 0.5,
+    opacity: 0,
+  },
+  "a"
+);
+
+tl.to(preloader, {
+  display: "none",
+});
+
+gsap.to("#text span", {
   xPercent: -100,
-  duration: 3,
+  duration: 10,
   ease: "linear",
   repeat: -1,
 });
-
 
 const text2 = document.getElementById("text2");
 
@@ -18,9 +92,4 @@ gsap.to(text2, {
   ease: "linear",
   repeat: -1,
 });
-
-
-
-
-
 
